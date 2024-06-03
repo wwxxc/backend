65ffa191dbbb3e06103348d2682d5a7f6d418b07
wwxxc <woianjur@gmail.com>
2024-06-03 13:11:43 +0700

    first commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Carousel from "@/components/Carousel";
+import Tabs from "@/components/Tabs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
   const images = [
@@ -26,21 +28,35 @@ export default function HomePage() {
               <p className="pl-6 text-xs"> 
                 Berikut adalah beberapa produk yang paling populer saat ini.
               </p>
-              <ul className="grid grid-cols-2 gap-4 md:grid-cols-3">
-                <li className="mt-2 p-2 bg-muted/30 rounded-xl flex ">
-                  <img className="rounded-xl" src="https://play-lh.googleusercontent.com/B2aIz3JtIQ57hLCFo-mw9iymuFEe7A2wUvJYAjr1cuV28T2bzkWmAH0EU4JCw3T8hg" alt="Mobile legends" width={50} />
-                  <h6 className="ml-3 text-xs">Mobile legends</h6>
+              <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 mt-5">
+                <Link href="/product/1">
+                <li className="mt-2 p-2 bg-muted/30 rounded-xl flex items-center hover:scale-105 transition-transform duration-300">
+                  <img className="rounded-xl w-full max-w-[60px] lg:max-w-[85px]" src="https://play-lh.googleusercontent.com/B2aIz3JtIQ57hLCFo-mw9iymuFEe7A2wUvJYAjr1cuV28T2bzkWmAH0EU4JCw3T8hg" alt="Mobile legends" />
+                  <div className="ml-3 text-white">
+                    <h6 className="text-xs font-bold">Mobile legends</h6>
+                    <p className="text-xs">Moonton</p>
+                  </div>
                 </li>
-                <li className="mt-2 p-2 bg-muted/30 rounded-xl flex">
-                  <img className="rounded-xl" src="https://play-lh.googleusercontent.com/B2aIz3JtIQ57hLCFo-mw9iymuFEe7A2wUvJYAjr1cuV28T2bzkWmAH0EU4JCw3T8hg" alt="Mobile legends" width={50} />
-                  <h6 className="ml-3 text-xs card-title">Mobile legends</h6>
+                </Link>
+                <li className="mt-2 p-2 bg-muted/30 rounded-xl flex items-center hover:scale-105 transition-transform duration-300">
+                  <img className="rounded-xl w-full max-w-[60px] lg:max-w-[85px]" src="https://play-lh.googleusercontent.com/B2aIz3JtIQ57hLCFo-mw9iymuFEe7A2wUvJYAjr1cuV28T2bzkWmAH0EU4JCw3T8hg" alt="Mobile legends" />
+                  <div className="ml-3 text-white">
+                    <h6 className="text-xs font-bold">Mobile legends</h6>
+                    <p className="text-xs">Moonton</p>
+                  </div>
                 </li>
-                <li className="mt-2 p-2 bg-muted/30 rounded-xl flex">
-                  <img className="rounded-xl" src="https://play-lh.googleusercontent.com/B2aIz3JtIQ57hLCFo-mw9iymuFEe7A2wUvJYAjr1cuV28T2bzkWmAH0EU4JCw3T8hg" alt="Mobile legends" width={50} />
-                  <h6 className="ml-3 text-xs card-title">Mobile legends</h6>
+                <li className="mt-2 p-2 bg-muted/30 rounded-xl flex items-center hover:scale-105 transition-transform duration-300">
+                  <img className="rounded-xl w-full max-w-[60px] lg:max-w-[85px]" src="https://play-lh.googleusercontent.com/B2aIz3JtIQ57hLCFo-mw9iymuFEe7A2wUvJYAjr1cuV28T2bzkWmAH0EU4JCw3T8hg" alt="Mobile legends" />
+                  <div className="ml-3 text-white">
+                    <h6 className="text-xs font-bold">Mobile legends</h6>
+                    <p className="text-xs">Moonton</p>
+                  </div>
                 </li>
               </ul>
             </div>
+          </div>
+          <div className="container">
+            <Tabs />
           </div>
         </div>
       </main>
---
+++ frontend/src/components/Tabs.tsx
@@ -0,0 +1,137 @@
+'use client'
+import React from "react";
+
+const Tabs = () => {
+  const [openTab, setOpenTab] = React.useState(1);
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <div className="flex flex-wrap overflow-x-auto">
+            <ul
+              className="flex mb-0 list-none pt-3 pb-4 flex-row"
+              role="tablist"
+              style={{ whiteSpace: "nowrap" }}
+            >
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded-md block leading-normal " +
+                    (openTab === 1
+                      ? "text-white bg-primary"
+                      : "text-grey-600 bg-muted/50")
+                  }
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTab(1);
+                  }}
+                  data-toggle="tab"
+                  href="#link1"
+                  role="tablist"
+                >
+                  TopUp Games
+                </a>
+              </li>
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                    (openTab === 2
+                      ? "text-white bg-primary"
+                      : "text-blueGray-600 bg-muted/50")
+                  }
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTab(2);
+                  }}
+                  data-toggle="tab"
+                  href="#link2"
+                  role="tablist"
+                >
+                  Voucher
+                </a>
+              </li>
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                    (openTab === 3
+                      ? "text-white bg-primary"
+                      : "text-blueGray-600 bg-muted/50")
+                  }
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTab(3);
+                  }}
+                  data-toggle="tab"
+                  href="#link3"
+                  role="tablist"
+                >
+                  E-Wallet
+                </a>
+              </li>
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                    (openTab === 4
+                      ? "text-white bg-primary"
+                      : "text-blueGray-600 bg-muted/50")
+                  }
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTab(4);
+                  }}
+                  data-toggle="tab"
+                  href="#link3"
+                  role="tablist"
+                >
+                  Pulsa & Data 
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="relative flex flex-col min-w-0 break-words bg-black w-full mb-6 shadow-lg rounded">
+            <div className="px-4 py-5 flex-auto">
+              <div className="tab-content tab-space">
+                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+                  <p>
+                    Collaboratively administrate empowered markets via
+                    plug-and-play networks. Dynamically procrastinate B2C users
+                    after installed base benefits.
+                    <br />
+                    <br /> Dramatically visualize customer directed convergence
+                    without revolutionary ROI.
+                  </p>
+                </div>
+                <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+                  <p>
+                    Completely synergize resource taxing relationships via
+                    premier niche markets. Professionally cultivate one-to-one
+                    customer service with robust ideas.
+                    <br />
+                    <br />
+                    Dynamically innovate resource-leveling customer service for
+                    state of the art customer service.
+                  </p>
+                </div>
+                <div className={openTab === 3 ? "block" : "hidden"} id="link3">
+                  <p>
+                    Efficiently unleash cross-media information without
+                    cross-media value. Quickly maximize timely deliverables for
+                    real-time schemas.
+                    <br />
+                    <br /> Dramatically maintain clicks-and-mortar solutions
+                    without functional solutions.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tabs;
