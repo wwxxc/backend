94915ad1feea0833fa820a2a977e1bc33dd220f5
wwxxc <woianjur@gmail.com>
2024-07-11 13:32:12 +0700

    first commit

--- frontend/src/app/[lang]/[slug]/page.tsx
+++ frontend/src/app/[lang]/[slug]/page.tsx
@@ -550,8 +550,14 @@ const DetailProduct = ({ params }: { params: { lang: string, slug: string} }) =>
                         <div className="sticky bottom-0 pb-4 flex flex-col gap-4 bg-background">
                         <div className="rounded-lg border border-dashed bg-secondary p-4 text-sm text-secondary-foreground">
                             <div className="flex items-center gap-4">
-                                <img src={product?.product_img} width={50} className="rounded" alt="" />
-                                <span className="text-xs">{selectedProduct?.name}</span>
+                                <div>
+                                    <img src={product?.product_img} alt="" className="rounded" width={50} />
+                                </div>
+                                <div>
+                                    <div className="text-sm font-medium">{selectedProduct?.name}</div>
+                                    <div className="text-sm text-foreground/50">{selectedProduct?.price.basic} {selectedPayment && '- ' + selectedPayment?.name}</div>
+                                    <span className="text-[0.625rem] italic text-card-foreground">**Proses Otomatis</span>
+                                </div>
                             </div>
                         </div>
                         <button type="submit" className="inline-flex items-center justify-center whitespace-nowrap text-xs font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-8 rounded-md px-3 w-full gap-2">
