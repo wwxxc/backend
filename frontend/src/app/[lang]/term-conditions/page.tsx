22b72cdbc080798d187ccf8cf4a34ef56c998093
wwxxc <woianjur@gmail.com>
2024-07-31 21:25:19 +0700

    first commit

--- frontend/src/app/[lang]/term-conditions/page.tsx
+++ frontend/src/app/[lang]/term-conditions/page.tsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function TermConditionPage() {
   return (
-    <div>TermConditionPage</div>
+    <main className='mt-16'>
+      <div className='container flex flex-col text-sm lg:text-md'>
+        <h1>Selamat datang di Kazupedia. Dengan menggunakan layanan kami, Anda setuju untuk mematuhi dan terikat oleh syarat dan ketentuan berikut. Harap baca dengan cermat sebelum menggunakan situs kami.</h1>
+        <br />
+        <h2>1. Penerimaan Syarat</h2>
+          <p>Dengan mengakses atau menggunakan situs Kazupedia, Anda setuju untuk terikat oleh syarat dan ketentuan ini. Jika Anda tidak setuju dengan bagian mana pun dari syarat ini, Anda tidak boleh menggunakan layanan kami.</p>
+        <br />
+        <h2>2. Layanan Kami</h2>
+          <p>Kazupedia menyediakan layanan topup game untuk berbagai platform. Kami berhak untuk mengubah atau menghentikan layanan kami sewaktu-waktu tanpa pemberitahuan sebelumnya.</p>
+        <br />
+        <h2>3. Penggunaan yang Diperbolehkan</h2>
+          <p>Anda setuju untuk menggunakan layanan kami hanya untuk tujuan yang sah dan sesuai dengan syarat ini. Anda tidak diperbolehkan menggunakan situs kami untuk aktivitas ilegal atau melanggar hak pihak ketiga.</p>
+        <br />
+        <h2>4. Akun Pengguna</h2>
+          <p>Anda bertanggung jawab untuk menjaga kerahasiaan informasi akun Anda dan bertanggung jawab penuh atas semua aktivitas yang terjadi di bawah akun Anda. Harap segera beri tahu kami jika terjadi pelanggaran keamanan atau penggunaan akun Anda tanpa izin.</p>
+        <br />
+        <h2>5. Pembayaran dan Transaksi</h2>
+          <p>Semua transaksi pembayaran harus dilakukan melalui metode pembayaran yang sah dan dapat dipercaya. Kami berhak menolak atau membatalkan transaksi atas kebijakan kami sendiri.</p>
+        <br />
+        <h2>7. Perubahan pada Syarat dan Ketentuan</h2>
+          <p>Kami dapat memperbarui syarat dan ketentuan ini sewaktu-waktu. Perubahan akan diberlakukan segera setelah dipublikasikan di situs kami. Penggunaan berkelanjutan Anda atas layanan kami setelah perubahan tersebut menandakan persetujuan Anda terhadap syarat yang diperbarui.</p>
+      </div>
+    </main>
   )
 }
 
