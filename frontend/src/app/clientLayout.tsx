4334a26f604e676035c491b53e0b88966edc26a1
wwxxc <woianjur@gmail.com>
2024-07-31 17:18:37 +0700

    first commit

--- frontend/src/app/clientLayout.tsx
+++ frontend/src/app/clientLayout.tsx
@@ -8,37 +8,21 @@ import axios from 'axios';
 
 export default function ClientLayout({
   children,
-}: Readonly<{
+  data
+}:{
   children: React.ReactNode;
-}>) {
+  data:Home
+}) {
   const pathname = usePathname();
   const hideNavbarAndFooter = pathname === '/id/sign-up' || pathname === '/id/sign-in' || pathname === '/404';
-  const [home, setHome] = useState<Home>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  useEffect(() => {
-    const fetchHome = async () => {
-      try {
-        const response = await axios.get(API_URL + '/home');
-        setHome(response.data);
-        setLoading(false);
-      } catch (error) {
-        // setLoading(false);
-        setError('Failed to fetch images');
-      }
-    }
-
-    fetchHome();
-  }, [API_URL]);
-  
 
   return (
     <>
       {!hideNavbarAndFooter && <Navbar />}
       {children}
-      {!hideNavbarAndFooter && home && <Footer {...home} />}
+      {!hideNavbarAndFooter && data && <Footer {...data} />}
     </>
   );
 }
