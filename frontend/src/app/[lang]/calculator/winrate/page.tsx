622f8b175245855f57563d35a0e3c2116a855766
wwxxc <woianjur@gmail.com>
2024-07-31 21:25:04 +0700

    first commit

--- frontend/src/app/[lang]/calculator/winrate/page.tsx
+++ frontend/src/app/[lang]/calculator/winrate/page.tsx
@@ -1,77 +1,36 @@
-'use client'
-import { useState } from "react";
-import Logo from "../../../../../public/assets/img/logo-2.png"
+import WinRatePage from "@/components/WinRate";
+import { Metadata, ResolvingMetadata } from "next";
+import { getDictionary } from "../../dictionaries";
 
-const Winrate = () => {
 
-    const [totalMatch, setTotalMatch] = useState(0);
-    const [totalWinrate, setTotalWinrate] = useState(0);
-    const [targetWinrate, setTargetWinrate] = useState(0);
-    const [targetWinrate2, setTargetWinrate2] = useState(0);
-    const [result, setResult] = useState(0);
-    const calculateWinrate = () => {
-        const res = rumus(totalMatch, totalWinrate, targetWinrate);
-        setResult(res);
-        setTargetWinrate2(targetWinrate);
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+ 
+export async function generateMetadata(
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+
+    const home: Home = await fetch(API_URL + '/home').then((res) => res.json())
+    const previousImages = (await parent).openGraph?.images || []
+    return {
+      title: 'Winrate Calculator' + ' / ' + home.title + ' - ' + home.description,
+      description: home.description,
+      keywords: home.title,
+      icons: {
+        icon: home.url_favicon,
+      },
+      openGraph: {
+        images: ['/.jpg', ...previousImages],
+      },
     }
-
-    function rumus(tMatch: number, tWr: number, wrReq: number) {
-        let tWin = tMatch * (tWr / 100);
-        let tLose = tMatch - tWin;
-        let sisaWr = 100 - wrReq;
-        let wrResult = 100 / sisaWr;
-        let seratusPersen = tLose * wrResult;
-        let final = seratusPersen - tMatch;
-        return Math.round(final);
-      }
-
+  }
+  
+const Winrate = async ({ params: { lang } }: { params: { lang: string } }) => {
+    const home: Home = await fetch(API_URL + '/home').then((res) => res.json())
+    const dict = await getDictionary(lang);
     return (
         <main>
-            <div className="mx-auto w-full max-w-xl space-y-8 px-4 pb-48 pt-24">
-                <div>
-                    <a href="">
-                        <img src={Logo.src} className="mx-auto h-32 w-auto" alt="" />
-                    </a>
-                    <h2 className="mt-2 text-center text-3xl font-bold tracking-tight text-foreground">
-                        Kalkulator Win Rate
-                    </h2>
-                    <p className="mt-2 text-center text-sm text-foreground">
-                        Digunakan untuk menghitung total jumlah match yang harus diambil untuk mencapai target Win rate yang diinginkan.
-                    </p>
-                </div>
-                <div className="mt-8 space-y-6">
-                    <div className="space-y-6 rounded-md shadow-sm">
-                        <div className="flex flex-col gap-y-2">
-                            <label htmlFor="total-match" className="block text-xs font-medium text-foreground">Total Pertandingan Anda Saat Ini</label>
-                            <div className="flex flex-col items-start">
-                                <input type="text" placeholder="Contoh: 223" required onChange={(e) => setTotalMatch(parseInt(e.target.value))} className="relative block w-full appearance-none rounded-lg border border-border bg-[#7F8487] px-3 py-2 text-xs text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-75" />
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-y-2">
-                            <label htmlFor="total-match" className="block text-xs font-medium text-foreground">Total Winrate Anda Saat Ini</label>
-                            <div className="flex flex-col items-start">
-                                <input type="text" placeholder="Contoh: 43" required onChange={(e) => setTotalWinrate(parseInt(e.target.value))} className="relative block w-full appearance-none rounded-lg border border-border bg-[#7F8487] px-3 py-2 text-xs text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-75" />
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-y-2">
-                            <label htmlFor="total-match" className="block text-xs font-medium text-foreground">Win Rate Total yang Anda Inginkan</label>
-                            <div className="flex flex-col items-start">
-                                <input type="text" placeholder="Contoh: 89" required onChange={(e) => setTargetWinrate(parseInt(e.target.value))} className="relative block w-full appearance-none rounded-lg border border-border bg-[#7F8487] px-3 py-2 text-xs text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-75" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-x-4">
-                        <button onClick={calculateWinrate} className="items-center justify-center rounded-lg bg-primary px-4 py-2 text-sm font-medium text-primary-foreground duration-300 hover:bg-primary/75 disabled:cursor-not-allowed disabled:opacity-75 group relative flex w-full">
-                            Hitung
-                        </button>
-                    </div>
-                        {result !== 0 &&(
-                            <div className="rounded-md border border-transparent bg-muted p-4 text-center text-sm font-semibold uppercase ring-1 ring-primary">
-                                You need about <strong className="text-primary">{result} Win without Lose</strong> to get a <strong className="text-primary">{targetWinrate2}% Win Rate</strong>
-                            </div>
-                        )}
-                </div>
-            </div>
+            <WinRatePage home={home} dict={dict} />
         </main>
     )
 }
--- frontend/src/app/[lang]/invoice/page.tsx
+++ frontend/src/app/[lang]/invoice/page.tsx
@@ -1,46 +1,37 @@
-'use client'
-import { Search } from "lucide-react"
-import { useState } from "react"
+import SearchInvoice from "@/components/SearchInvoice";
+import { ResolvingMetadata, Metadata } from "next";
+import { getDictionary } from "../dictionaries";
 type Props = {
     params: { [lang: string]: string };
   };
 
-const InvoicePage = ({ params: { lang } }: Props) => {
-    const [invoice, setInvoice] = useState('')
-    const [isLoading, setIsLoading] = useState(false)    
-    const fetchInvoice = async () => {
-        window.location.href = `/${lang}` + '/invoice/' + invoice
-    }
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-    const submitbtn = () => {
-        if (invoice !== '') {
-            setIsLoading(true)
-            fetchInvoice()
-        }
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+
+    const home: Home = await fetch(API_URL + '/home').then((res) => res.json())
+    const previousImages = (await parent).openGraph?.images || []
+    return {
+      title: 'Search Invoice ' + ' / ' + home.title + ' - ' + home.description,
+      description: home.description,
+      keywords: home.title,
+      icons: {
+        icon: home.url_favicon,
+      },
+      openGraph: {
+        images: ['/.jpg', ...previousImages],
+      },
     }
-    
+  }
+
+const InvoicePage = async ({ params: { lang } }: Props) => {
+    const dict = await getDictionary(lang);
     return ( 
         <section className="bg-[#393E46]">
-            <div className="space-y-12">
-                <div className="relative overflow-hidden shadow-2xl">
-
-                </div>
-                <div className="container relative z-20 py-12 text-left">
-                    <h2 className="max-w-2xl text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
-                        Cari Pesanan
-                    </h2>
-                    <p className="mt-3 max-w-3xl text-foreground">Lacak transaksi kamu dengan cara memasukkan Nomor Invoice dibawah ini:</p>
-                    <div className="mt-6 max-w-xl">
-                        <label htmlFor="invoice" className="block text-xs font-medium text-foreground mb-2 text-left">Nomor Invoice Kamu</label>
-                        <div className="flex flex-col items-start">
-                            <input type="text" placeholder="EPOXXXXXXXXXXXX" name="" id="" onChange={(e) => setInvoice(e.target.value)} className="relative block w-full appearance-none rounded-lg border border-border bg-[#7F8487] px-3 py-2 text-xs text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary disabled:cursor-not-allowed disabled:opacity-75 " />
-                        </div>
-                    </div>
-                    <div className="mt-6 flex items-center justify-start gap-x-6">
-                        <button disabled={isLoading} onClick={submitbtn} className="inline-flex items-center justify-center rounded-lg bg-primary px-4 py-2 text-sm font-medium text-primary-foreground duration-300 hover:bg-primary/75 disabled:cursor-not-allowed disabled:opacity-75 space-x-2 !pl-3 !pr-4"><Search size={18} className="mr-2" /> {isLoading ? ' Loading...' : ' Cari Transaksi'}</button>
-                    </div>
-                </div>
-            </div>
+            <SearchInvoice lang={lang} dict={dict}/>
         </section>
     )
 }
