a7b01349d2018776284cfe15182830b01d130a69
wwxxc <woianjur@gmail.com>
2024-08-01 23:03:33 +0700

    first commit

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,23 +1,57 @@
-const expres = require('express');
-const router = expres.Router();
+const express = require('express');
+const router = express.Router();
 const Product = require('../models/Product');
 
+// Get all products
 router.get('/', async (req, res) => {
-    try {
-        const products = await Product.findAll();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+// Get a product by slug
 router.post('/:id', async (req, res) => {
-    try {
-        const products = await Product.findOne({ where: { product_slug: req.params.id } });
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const product = await Product.findOne({ where: { product_slug: req.params.id } });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
+// Update a product
+router.put('/:id', async (req, res) => {
+  try {
+    const product = await Product.findOne({ where: { product_slug: req.params.id } });
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    const updatedProduct = await product.update(req.body);
+    res.json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a product
+router.delete('/:id', async (req, res) => {
+    try {
+      const product = await Product.findOne({ where: { product_slug: req.params.id } });
+      if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+      }
+  
+      await product.destroy();
+      res.status(204).end(); // No content to send back
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
 
 module.exports = router;
